046e334de715975d905d2b493dbc1b115cacc16e
Ethan Casteel <casteelk@gmail.com>
2021-06-30 12:37:36 -0400

    branch for CI

--- pkg/api/enterprise.gloo.solo.io/v1/auth_config.pb.equal.go
+++ pkg/api/enterprise.gloo.solo.io/v1/auth_config.pb.equal.go
@@ -985,6 +985,10 @@ func (m *OidcAuthorizationCode) Equal(that interface{}) bool {
 		}
 	}
 
+	if strings.Compare(m.GetSessionIdHeaderName(), target.GetSessionIdHeaderName()) != 0 {
+		return false
+	}
+
 	return true
 }
 
@@ -2453,6 +2457,10 @@ func (m *ExtAuthConfig_OidcAuthorizationCodeConfig) Equal(that interface{}) bool
 		if !proto.Equal(m.GetJwksCacheRefreshPolicy(), target.GetJwksCacheRefreshPolicy()) {
 			return false
 		}
+	}
+
+	if strings.Compare(m.GetSessionIdHeaderName(), target.GetSessionIdHeaderName()) != 0 {
+		return false
 	}
 
 	return true
