ffbe53609db1b786ff08b82096e1fcc728979fde
Sam Heilbron <samheilbron@gmail.com>
2021-09-07 08:05:37 -0400

    update ratelimit and codegen

--- pkg/api/ratelimit.solo.io/v1alpha1/ratelimit.pb.equal.go
+++ pkg/api/ratelimit.solo.io/v1alpha1/ratelimit.pb.equal.go
@@ -102,6 +102,47 @@ func (m *RateLimitConfigStatus) Equal(that interface{}) bool {
 }
 
 // Equal function
+func (m *RateLimitConfigNamespacedStatuses) Equal(that interface{}) bool {
+	if that == nil {
+		return m == nil
+	}
+
+	target, ok := that.(*RateLimitConfigNamespacedStatuses)
+	if !ok {
+		that2, ok := that.(RateLimitConfigNamespacedStatuses)
+		if ok {
+			target = &that2
+		} else {
+			return false
+		}
+	}
+	if target == nil {
+		return m == nil
+	} else if m == nil {
+		return false
+	}
+
+	if len(m.GetStatuses()) != len(target.GetStatuses()) {
+		return false
+	}
+	for k, v := range m.GetStatuses() {
+
+		if h, ok := interface{}(v).(equality.Equalizer); ok {
+			if !h.Equal(target.GetStatuses()[k]) {
+				return false
+			}
+		} else {
+			if !proto.Equal(v, target.GetStatuses()[k]) {
+				return false
+			}
+		}
+
+	}
+
+	return true
+}
+
+// Equal function
 func (m *Descriptor) Equal(that interface{}) bool {
 	if that == nil {
 		return m == nil
