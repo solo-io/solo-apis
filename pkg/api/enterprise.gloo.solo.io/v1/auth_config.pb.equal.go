6a34d8ef692ac6bf0dcf14b4d4cba305c20a747d
JM Huibonhoa <jm.huibonhoa@solo.io>
2022-07-20 09:29:08 -0400

    this branch is used for testing only, ran make generate to generate codegen changes for proto change

--- pkg/api/enterprise.gloo.solo.io/v1/auth_config.pb.equal.go
+++ pkg/api/enterprise.gloo.solo.io/v1/auth_config.pb.equal.go
@@ -2399,6 +2399,16 @@ func (m *UserSession_InternalSession) Equal(that interface{}) bool {
 		return false
 	}
 
+	if h, ok := interface{}(m.GetAllowRefreshing()).(equality.Equalizer); ok {
+		if !h.Equal(target.GetAllowRefreshing()) {
+			return false
+		}
+	} else {
+		if !proto.Equal(m.GetAllowRefreshing(), target.GetAllowRefreshing()) {
+			return false
+		}
+	}
+
 	return true
 }
 
