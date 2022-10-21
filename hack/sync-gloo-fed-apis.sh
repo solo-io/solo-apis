4f094fa314dd9a134aaf5407f5f8421022ca7185
Jenny Shu <jenny.shu@solo.io>
2022-10-21 08:48:26 -0400

    update scripts

--- hack/sync-gloo-fed-apis.sh
+++ hack/sync-gloo-fed-apis.sh
@@ -4,14 +4,11 @@ set -e
 
 rsync -ax ../solo-projects/projects/gloo-fed/api/  ./api/gloo-fed
 
+# Change the paths in the Gloo Fed protos from solo-projects to solo-apis
 for file in $(find api/gloo-fed -type f | grep ".proto")
 do
-
-# Gloo Fed API changes
-  sed "s|github.com/solo-io/solo-projects/projects/gloo-fed/pkg/api/fed|github.com/solo-io/solo-apis/pkg/api/fed|g" "$file" > "$file".tmp && mv "$file".tmp "$file"
-  sed "s|github.com/solo-io/solo-projects/projects/gloo-fed/api/fed|github.com/solo-io/solo-apis/api/gloo-fed/fed|g" "$file" > "$file".tmp && mv "$file".tmp "$file"
-
-# Skv2 Enterprise Api changes
-  sed "s|github.com/solo-io/skv2-enterprise/multicluster-admission-webhook/api/multicluster|github.com/solo-io/solo-apis/api/skv2-enterprise|g" "$file" > "$file".tmp && mv "$file".tmp "$file"
-  
+  # rename the go_package from fed protos to have a solo-apis path
+  sed "s|github.com/solo-io/solo-projects/projects/gloo-fed/pkg/api/|github.com/solo-io/solo-apis/pkg/api/|g" "$file" > "$file".tmp && mv "$file".tmp "$file"
+  # rename the proto imports to refer to the solo-apis path
+  sed "s|github.com/solo-io/solo-projects/projects/gloo-fed/api/|github.com/solo-io/solo-apis/api/gloo-fed/|g" "$file" > "$file".tmp && mv "$file".tmp "$file"
 done
